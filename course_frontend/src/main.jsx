2c1821bd55552f0d68bc69326b4c5cd2340a6a6d
Jennifer Osato <st.jennyandy@gmail.com>
2024-08-19 03:35:50 +0100

    auth pages

---
+++ course_frontend/src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import SignUp from "./components/SignUp.jsx";
+import Login from "./components/Login.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
